622aaba976335ecf6c81451dca7edddb558ddc53
Li Tianshui <35996450+LiTianshui@users.noreply.github.com>
2021-04-11 10:03:10 +0900

    Add files via upload

---
+++ algorithm/books/ACM_ICPC算法基础训练教程/第1章 基础算法/1_12.cpp
@@ -0,0 +1,25 @@
+// 1-12 Rightmost Digit
+// 2021-04-09
+// 快速幂取模算法详解：	 https://www.cnblogs.com/zjl192628928/p/9382752.html
+// 快速幂取模算法：		 https://www.cnblogs.com/wuyudong/p/3637479.html
+
+#include <iostream>
+using namespace std;
+long long mod(long long a, long long b)
+{
+	if (b == 1) return a;
+	long long s = mod(a, b/2) % 10;		// 把问题分成两部分，恰好这两部分工作是相同的
+	if (b % 2 == 0) return (s * s) % 10;
+	else return (s * s * a) % 10; 
+}
+int main()
+{
+	int t, N;
+	cin >> t;
+	while (t--)
+	{
+		cin >> N;
+		cout << mod(N, N) << endl;
+	}
+	return 0;
+}
