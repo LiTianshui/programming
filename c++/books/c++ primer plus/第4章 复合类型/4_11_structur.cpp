071d84e687182421f2980613b9b307b0aa80f096
Li Tianshui <35996450+LiTianshui@users.noreply.github.com>
2021-05-03 14:54:57 +0900

    Add files via upload

---
+++ c++/books/c++ primer plus/第4章 复合类型/4_11_structur.cpp
@@ -0,0 +1,33 @@
+// structur.cpp -- a simple structure
+// 2020-03-31
+
+#include <iostream>
+struct inflatable	// structure declaration
+{
+	char name[20];
+	float volume;
+	double price;
+};
+
+int main()
+{
+	using namespace std;
+	inflatable guest = 
+	{
+		"Glorious Gloria",	// name value
+		1.88,				// volume value
+		29.99				// price value
+	};	// guest is a structure variable of type inflatable
+	inflatable pal =
+	{
+		"Audacious Arthur",
+		3.12,
+		32.99
+	};	// pal is a second variable of type inflatable
+	
+	cout << "Expand your guest list with " << guest.name;
+	cout << " and " << pal.name << "!\n";
+	cout << "You can have both for $";
+	cout << guest.price + pal.price << "!\n";
+	return 0;
+}
