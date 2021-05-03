2e6f6728e1b97cae9841b1a48fd3fad36975c81e
Li Tianshui <35996450+LiTianshui@users.noreply.github.com>
2021-05-03 14:55:57 +0900

    Add files via upload

---
+++ c++/books/c++ primer plus/第4章 复合类型/4_14_address.cpp
@@ -0,0 +1,18 @@
+// address.cpp -- using a operator to find addresses
+// 2020-09-06
+
+#include <iostream>
+int main()
+{
+	using namespace std;
+	int donuts = 6;
+	double cups = 4.5;
+	
+	cout << "donuts value = " << donuts;
+	cout << " and donuts address = " << &donuts << endl;
+// NOTE: you may need to use unsigned (&donute)
+// and unsigned (&cups)
+	cout << "cups value = " << cups;
+	cout << " and cups address = " << &cups << endl;
+	return 0;
+}
