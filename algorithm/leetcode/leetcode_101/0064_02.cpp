12eff992b140859dabfdc0fe28baa4b3032d50ff
Li Tianshui <35996450+LiTianshui@users.noreply.github.com>
2021-06-30 17:51:00 +0900

    Add files via upload

---
+++ algorithm/leetcode/leetcode_101/0064_02.cpp
@@ -0,0 +1,21 @@
+// 2021-06-30
+// 64. 最小路径和 （压缩为一维）
+
+int minPathSum(vector<vector<int>> &grid) {
+	int m = grid.size(), n = grid[0].size();
+	vector<int> dp(n, 0);
+	for (int i = 0; i < m; ++i) {
+		for (int j = 0; j < n; ++j) {
+			if (i == 0 && j == 0) {
+				dp[j] = grid[i][j];
+			} else if (i == 0) {
+				dp[j] = dp[j - 1] + grid[i][j];
+			} else if (j == 0) {
+				dp[j] = dp[j] + grid[i][j];
+			} else {
+				dp[j] = min(dp[j], dp[j - 1]) + grid[i][j];
+			}
+		}
+	}
+	return dp[n - 1];
+}
