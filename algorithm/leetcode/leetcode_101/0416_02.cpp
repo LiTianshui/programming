b9f9f525db1b168af7a22439d72075d6039e7450
Li Tianshui <35996450+LiTianshui@users.noreply.github.com>
2021-07-07 19:22:14 +0900

    Add files via upload

---
+++ algorithm/leetcode/leetcode_101/0416_02.cpp
@@ -0,0 +1,16 @@
+// 2021-07-07
+// 416. 分割等和子集 （空间压缩）
+
+bool canPartition(vector<int> &nums) {
+	int sum = accumulate(nums.begin(), nums.end(), 0);
+	if (sum % 2) return false;
+	int target = sum / 2, n = nums.size();
+	vector<bool> dp(target + 1, false);
+	dp[0] = true;
+	for (int i = 1; i <= n; ++i) {
+		for (int j = target; j >= nums[i - 1]; --j) {
+			dp[j] = dp[j] || dp[j - nums[i - 1]];
+		}
+	}
+	return dp[target];
+} 
