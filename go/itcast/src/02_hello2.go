7953deb692f212b202d6da2178fbd065b807dd74
Li Tianshui <35996450+LiTianshui@users.noreply.github.com>
2021-11-20 16:06:30 +0800

    Delete go/itcast/src directory

--- go/itcast/src/02_hello2.go
+++
@@ -1,8 +0,0 @@
-// 2021-11-12
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println("hello mike")
-}
--- go/itcast/src/04_自动推导类型.go
+++
@@ -1,9 +0,0 @@
-// 2021-11-12
-package main
-
-import "fmt"
-
-func main() {
-	a := 10
-	fmt.Println("a = ", a)
-}
