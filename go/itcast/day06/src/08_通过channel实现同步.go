5e29f21fdd6c6dcdf68aa29e7fd880d7c69c0de3
Li Tianshui <35996450+LiTianshui@users.noreply.github.com>
2021-12-01 13:06:57 +0800

    Add files via upload

---
+++ go/itcast/day06/src/08_通过channel实现同步.go
@@ -0,0 +1,41 @@
+// 2021-12-01
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// 全局变量，创建一个channel
+var ch = make(chan int)
+
+// 定义一个打印机，参数为字符串，按每个字符打印
+// 打印机属于公共资源
+func Printer(str string) {
+	for _, data := range str {
+		fmt.Printf("%c", data)
+		time.Sleep(time.Second)
+	}
+	fmt.Printf("\n")
+}
+
+// person1执行完后，才能到person2执行
+func person1() {
+	Printer("hello")
+	ch <- 666 // 给管道写数据，发送
+}
+
+func person2() {
+	<-ch // 从管道取数据，接收，如果通道没有数据它就会阻塞
+	Printer("world")
+}
+
+func main() {
+	// 新建2个协程，代表2个人同时使用打印机
+	go person1()
+	go person2()
+
+	for {
+
+	}
+}
