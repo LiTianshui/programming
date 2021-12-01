eb9b0ebb92f1b4303835a67cedafb780412911a6
Li Tianshui <35996450+LiTianshui@users.noreply.github.com>
2021-12-01 21:16:09 +0800

    Add files via upload

---
+++ go/itcast/day06/src/12_关闭channel.go
@@ -0,0 +1,49 @@
+// 2021-12-01
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	ch := make(chan int) // 创建一个无缓冲channel
+
+	// 新建一个goroutine
+	go func() {
+		for i := 0; i < 5; i++ {
+			ch <- i // 往通道写数据
+		}
+		// 不需要再写数据时，关闭channel
+		close(ch)
+		// ch <- 666	// 关闭channel后无法再发送数据
+	}() // 别忘了()
+
+	for num := range ch {
+		fmt.Println("num = ", num)
+	}
+}
+
+func main01() {
+
+	ch := make(chan int) // 创建一个无缓冲channel
+
+	// 新建一个goroutine
+	go func() {
+		for i := 0; i < 5; i++ {
+			ch <- i // 往通道写数据
+		}
+		// 不需要再写数据时，关闭channel
+		close(ch)
+		// ch <- 666	// 关闭channel后无法再发送数据
+	}() // 别忘了()
+
+	for {
+		// 如果ok为true，说明管道没有关闭
+		if num, ok := <-ch; ok == true {
+			fmt.Println("num = ", num)
+		} else { // 管道关闭
+			break
+		}
+	}
+}
---
+++ go/itcast/day06/src/17_停止和重置定时器.go
@@ -0,0 +1,21 @@
+// 2021-12-01
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	timer := time.NewTimer(3 * time.Second)
+
+	go func() {
+		<-timer.C
+		fmt.Println("子协程可以打印了，因为定时器的时间到")
+	}()
+
+	timer.Stop() // 停止定时器
+	for {
+
+	}
+}
