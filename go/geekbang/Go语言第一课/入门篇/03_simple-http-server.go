aa8fd0a6e5438dba58c032ed57a02992ed333943
Li Tianshui <35996450+LiTianshui@users.noreply.github.com>
2022-01-20 18:24:30 +0800

    Add files via upload

---
+++ go/geekbang/Go语言第一课/入门篇/03_simple-http-server.go
@@ -0,0 +1,11 @@
+// 2022-01-20
+package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello, world"))
+	})
+	http.ListenAndServe(":8080", nil)
+}
