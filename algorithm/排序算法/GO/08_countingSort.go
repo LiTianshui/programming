f94522988b89c99ea651d495aedb9360b04052e1
Li Tianshui <35996450+LiTianshui@users.noreply.github.com>
2022-05-18 19:54:57 +0800

    Add files via upload

---
+++ algorithm/排序算法/GO/08_countingSort.go
@@ -0,0 +1,26 @@
+// 2022-05-18
+// 计数排序
+
+func countingSort(arr []int) []int {
+	length := len(arr)
+	maxValue := arr[0]
+	for i := 1; i < length; i++ {
+		if maxValue < arr[i] {
+			maxValue = arr[i]
+		}
+	}
+	bucketLen := maxValue + 1
+	bucket := make([]int, bucketLen)
+	sortedIndex := 0
+	for i := 0; i < length; i++ {
+		bucket[arr[i]]++
+	}
+	for j := 0; j < bucketLen; j++ {
+		for bucket[j] > 0 {
+			arr[sortIndex] = j
+			sortedIndex++
+			bucket[j]--
+		}
+	}
+	return arr
+}
